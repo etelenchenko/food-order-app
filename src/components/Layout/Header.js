050cc385930a276ebe34a1d801aea10427b849cf
eugene.telenchenko <etelenchenko@gmail.com>
2022-09-27 15:43:24 +0300

    added header, icon, cart button

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import classes from "./Header.module.css";
+
+import mealsImage from "../../assets/img.png";
+import HeaderCartButton from "./HeaderCartButton";
+
+function Header(props) {
+  return (
+    <React.Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton/>
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt="A table full of delicious food"/>
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default Header;
