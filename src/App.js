7f3599b3f8ec41765e05d73f22ea8538b6403be2
eugene.telenchenko <etelenchenko@gmail.com>
2022-09-28 12:56:17 +0300

    added context api for cart

--- src/App.js
+++ src/App.js
@@ -3,24 +3,22 @@ import './App.css';
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
 
   const showCartHandler = () => setCartIsShown(true);
-  const hideCartHandler = () => {
-    console.log("hideCartHandler");
-    setCartIsShown(false);
-  };
+  const hideCartHandler = () => setCartIsShown(false);
 
   return (
-    <React.Fragment>
+    <CartProvider>
       {cartIsShown && <Cart onClose={hideCartHandler}/>}
       <Header onShowCart={showCartHandler}/>
       <main>
         <Meals/>
       </main>
-    </React.Fragment>
+    </CartProvider>
   );
 }
 
