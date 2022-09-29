853729ba30c0c0660cdaea8e4b384994c58a3168
eugene.telenchenko <etelenchenko@gmail.com>
2022-09-29 14:20:08 +0300

    added possibility to add and remove items in the cart itself

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -7,9 +7,39 @@ const defaultCartState = {
 };
 
 const cartReducer = (state, action) => {
+  let updatedItems;
+  let updatedItem;
   if (action.type === "ADD") {
-    const updatedItems = state.items.concat(action.item);
+    const existingCartItemIndex = state.items.findIndex(item => item.id === action.item.id);
+    const existingCartItem = state.items[existingCartItemIndex];
+    if (existingCartItem) {
+      updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount
+      }
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(action.item);
+    }
+
     const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount
+    };
+  }
+  if (action.type === "REMOVE") {
+    const existingCartItemIndex = state.items.findIndex(item => item.id === action.id);
+    const existingCartItem = state.items[existingCartItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingCartItem.price;
+    if (existingCartItem.amount === 1) {
+      updatedItems = state.items.filter(item => item.id !== action.id)
+    } else {
+      updatedItem = {...existingCartItem, amount: existingCartItem.amount - 1};
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount
